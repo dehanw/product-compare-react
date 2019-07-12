613b12a41ae16b8e9703ba08a7e2fb7ea34cd6b8
dwang <dehan.wang@autogeneral.com.au>
2019-07-12 11:11:39 +1000

    applitools eyes

--- cypress/integration/compare.spec.js
+++ cypress/integration/compare.spec.js
@@ -6,11 +6,21 @@ describe("Product Comparison", () => {
     });
 
     it("selects two products to compare them", () => {
+        // Start the test
+        cy.eyesOpen({
+            appName: "ProductCompare",
+            testName: "Select 2 products to compare",
+            browser: { width: 800, height: 600 }
+        });
+
         // Click the Cherry Product
         cy.get("[data-cy=one-product]")
             .find("[data-cy=view-details]")
             .eq(0)
             .click();
+
+        // Take a snapshot
+        cy.eyesCheckWindow("first product selected");
 
         // Click the Orange Product
         cy.get("[data-cy=one-product]")
@@ -18,10 +28,16 @@ describe("Product Comparison", () => {
             .eq(1)
             .click();
 
+        // Take a snapshot
+        cy.eyesCheckWindow("second product selected");
+
         // Verify Cherry was added to the comparison list
         cy.get("[data-cy=product-compare]").contains("Cherry");
 
         // Verify Orange was added to the comparison list
         cy.get("[data-cy=product-compare]").contains("Orange");
+
+        // End Test
+        cy.eyesClose();
     });
 });
